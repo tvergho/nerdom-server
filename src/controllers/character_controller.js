ea60b2e882d89b172e6996238ce0d37100a7cfd5
tvergho <62906594+tvergho@users.noreply.github.com>
2020-05-16 02:35:14 -0700

    Typo

--- src/controllers/character_controller.js
+++ src/controllers/character_controller.js
@@ -106,13 +106,13 @@ export const updateRankings = async (req, res) => {
 
   if (diff1 > 0) { // Unexpected loss.
     await Character.findByIdAndUpdate(winner._id, { score: winner.score + (diff1 * 2.6) });
-    await Character.findByIdAndUpdate(loser1._id, { score: Math.max(winner.score - (diff1 * 3.5), winner.score - 10) });
+    await Character.findByIdAndUpdate(loser1._id, { score: Math.max(loser1.score - (diff1 * 3.5), loser1.score - 10) });
   } else { // Expected win.
     await Character.findByIdAndUpdate(winner._id, { score: winner.score + 1 });
   }
   if (diff2 > 0) {
     await Character.findByIdAndUpdate(winner._id, { score: winner.score + (diff2 * 2.6) });
-    await Character.findByIdAndUpdate(loser2._id, { score: Math.max(winner.score - (diff1 * 3.5), winner.score - 10) });
+    await Character.findByIdAndUpdate(loser2._id, { score: Math.max(loser2.score - (diff1 * 3.5), loser2.score - 10) });
   } else {
     await Character.findByIdAndUpdate(winner._id, { score: winner.score + 1 });
   }
